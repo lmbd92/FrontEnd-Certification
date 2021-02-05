ee507ef1f83870b0d1d26188b144c0b34d99f3e5
Lina Beltrán <lmbd92@hotmail.com>
2021-02-05 18:35:09 -0500

    Inicio Modulo 2 Js

---
+++ Modulo-2-JavaScript/Clase26/app.js
@@ -0,0 +1,14 @@
+"use strict";
+/* var nombre = prompt("Por favor ingrese su nombre:");
+var apellido = prompt("Por favor ingrese su apellido:");
+
+console.log("Tu nombre es " + nombre + " " + apellido);
+console.log(`Tu nombre es ${nombre} ${apellido}`); */
+
+var num1 = 9,
+    num2 = 3;
+
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 + num2);
+console.log(num1 - num2);
