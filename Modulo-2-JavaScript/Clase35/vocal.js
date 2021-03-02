9783bb3e74b0b9aaf123ad7fa9656506670ed4be
Lina Beltrán <lmbd92@hotmail.com>
2021-03-01 20:15:28 -0500

    Funcion esVocal

--- Modulo-2-JavaScript/Clase35/vocal.js
+++ Modulo-2-JavaScript/Clase35/vocal.js
@@ -1,8 +1,36 @@
-'use strict';
+"use strict";
 
 let letra = prompt("Ingrese una letra");
 
-if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"){
+/* if (
+  letra == "a" ||
+  letra == "e" ||
+  letra == "i" ||
+  letra == "o" ||
+  letra == "u"
+) {
+  console.log("Es vocal");
+} else {
+  console.log("No es vocal");
+} */
+
+const esVocal = (letra) => {
+  if (
+    letra == "a" ||
+    letra == "e" ||
+    letra == "i" ||
+    letra == "o" ||
+    letra == "u"
+  ) {
+    return true
+  } else {
+    return false
+  }
+};
+
+const validar = esVocal(letra);
+
+if (validar){
     console.log("Es vocal");
 }else{
     console.log("No es vocal");
