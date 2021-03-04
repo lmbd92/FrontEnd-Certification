a529268e2ad5e9d130368d643bc538781c5e0857
Lina Beltrán <lmbd92@hotmail.com>
2021-03-03 19:51:18 -0500

    Ejercicios con el DOM

--- Modulo-2-JavaScript/Clase36/DOM/Ej2/js/app.js
+++ Modulo-2-JavaScript/Clase36/DOM/Ej2/js/app.js
@@ -1,6 +1,20 @@
 'use strict';
 
-const fondo = prompt("Ingrese un color de fondo en hexadecimal" + "\n" + "No olvide incluir el # al inicio del color");
+/* const fondo = prompt("Ingrese un color de fondo en hexadecimal" + "\n" + "No olvide incluir el # al inicio del color"); */
 
+//Capturo el elemento DOM basado en el id "ini"
+const id = document.getElementById("ini");
+//Modifico el texto del DOM (h1)
+id.innerHTML = "Este es el elegido";
+
+
+//Remuevo la clase aplicada en el HTML
+id.classList.remove("red");
+//Le aplico una nueva clase del CSS
+id.classList.add("green");
+
+//Capturo el elemento DOM basado en la etiqueta "body"
 const body = document.querySelector("body");
+
+//Modifico el style de la etiqueta body con ayuda del metodo style
 body.style = `background-color: ${fondo}`;
